21fdd55eda237f88ac64b5cbd8485905c3f9aaf1
Huybe2711 <155123412+Huybe2711@users.noreply.github.com>
2024-01-03 09:18:24 +0700

    Add files via upload

---
+++ BT1/BT1/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaiTap1
+{
+    class Rectangle
+    {
+        private double length;
+        private double width;
+        public Rectangle()
+        {
+            length = 1.0;
+            width = 1.0;
+        }
+        public Rectangle(double length, double width)
+        {
+            this.length = length;
+            this.width = width;
+        }
+        public void setLength(double length)
+        {
+            this.length = length;
+        }
+        public double getLength()
+        {
+            return length;
+        }
+        public void setWidth(double width)
+        {
+            this.width = width;
+        }
+        public double getWidth()
+        {
+            return width;
+        }
+        public double findArea()
+        {
+            return length * width;
+        }
+        public double findPerimeter()
+        {
+            return (length + width) * 2;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Rectangle r1;
+            Rectangle r2;
+            r1 = new Rectangle();
+
+            Console.Write("Cho biết chiều dài:");
+            double length = double.Parse(Console.ReadLine());
+            Console.Write("Cho biết chiều rộng:");
+            double width = double.Parse(Console.ReadLine());
+
+            r2 = new Rectangle(length, width);
+            Console.WriteLine("----------------------Thông tin hình chữ nhật r1-----------------------");
+            Console.WriteLine("\tChiều dài: {0}\t\tChiều rộng: {1}\n\tDiện tích: {2}\t\tChu vi: {3}", r1.getLength(), r1.getWidth(), r1.findArea(), r1.findPerimeter());
+
+            Console.WriteLine("----------------------Thông tin hình chữ nhật r2-----------------------");
+            Console.WriteLine("\tChiều dài: {0}\t\tChiều rộng: {1}\n\tDiện tích: {2}\t\tChu vi: {3}", r2.getLength(), r2.getWidth(), r2.findArea(), r2.findPerimeter());
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ BT1/BT3/Program.cs
@@ -0,0 +1,109 @@
+﻿Bật thông báo trên màn hình cho Gmail.
+   OK  Không, cảm ơn
+1 trong tổng số 258
+(không có chủ đề)
+Hộp thư đến
+
+K PĂ Y SAN HY C23A_TH1 <sanhysky@gmail.com>
+Tệp đính kèm
+08:12(0 phút trước)
+đến tôi
+
+
+ 2 tệp đính kèm
+  • Gmail đã quét
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BT3
+{
+    class Phanso
+    {
+        private int tuso;
+        private int mauso;
+        public Phanso()
+        {
+            tuso = 0;
+            mauso = 1;
+        }
+        public Phanso(int tuso, int mauso)
+        {
+            this.tuso = tuso;
+            this.mauso = mauso;
+        }
+        public void settuso(int tuso)
+        {
+            this.tuso = tuso;
+        }
+        public int gettuso()
+        {
+            return tuso;
+        }
+        public void setmauso(int mauso)
+        {
+            this.mauso = mauso;
+        }
+        public int getmauso()
+        {
+            return mauso;
+        }
+        public void Toigian()
+        {
+            int uscln = TimUSCLN(tuso, mauso);
+            tuso = tuso / uscln;
+            mauso = mauso / uscln;
+        }
+        private int TimUSCLN(int a, int b)
+        {
+            int x = Math.Abs(a);
+            int y = Math.Abs(b);
+            while (x != y)
+            {
+                if (x > y)
+                    x = x - y;
+                else
+                    y = y - x;
+            }
+            return x;
+        }
+        public Phanso cong(Phanso ps)
+        {
+            Phanso kq = new Phanso();
+            kq.tuso = tuso * ps.mauso + ps.tuso * mauso;
+            kq.mauso = mauso * ps.mauso;
+            return kq;
+        }
+        public Phanso tru(Phanso ps)
+        {
+            Phanso kq = new Phanso();
+            kq.tuso = tuso * ps.mauso;
+            kq.mauso = mauso * ps.mauso;
+            return kq;
+        }
+        public Phanso nhan(Phanso ps)
+        {
+            Phanso kq = new Phanso();
+            kq.tuso = tuso * ps.mauso;
+            kq.mauso = mauso * ps.mauso;
+            return kq;
+        }
+        public Phanso chia(Phanso ps)
+        {
+            Phanso kq = new Phanso();
+            kq.tuso = tuso * ps.mauso;
+            kq.mauso = mauso * ps.tuso;
+            return kq;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
