92bb0b7c465294e7cce51a39c0e1220fde44c8c7
Huybe2711 <155123412+Huybe2711@users.noreply.github.com>
2024-01-05 11:28:08 +0700

    Add files via upload

---
+++ BT1/BT05/BT05/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BT05
+{
+    class CD
+    {
+        private int macd;
+        private string tuacd;
+        private string casi;
+        private int sobaihat;
+        private int dongia;
+        public CD(int macd)
+        {
+            this.macd = macd;
+        }
+        public CD(int macd, string tuacd, string casi, int sobaihat, int dongia)
+        {
+            this.macd = macd;
+            this.tuacd = tuacd;
+            this.casi = casi;
+            this.sobaihat = sobaihat;
+            this.dongia = dongia;
+
+        }
+        public int MaCD
+        {
+            set { this.macd = value; }
+            get { return macd; }
+        }
+        public string TuaCD
+        {
+            set { this.tuacd = value;}
+            get { return tuacd; }
+        }
+        public string Casi
+        {
+            set { this.casi = value; }
+            get { return casi; }
+        } 
+        public int SoBaiHat
+        {
+            set { this.sobaihat = value; }
+            get { return sobaihat; }
+
+        }
+        public int DonGia
+        {
+            set { this.dongia = value; }
+            get { return dongia; }
+        }
+    }
+}
